27a22592197849263351e4d632577d3713bb42ec
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-18 13:18:59 +0200

    print_game.c

--- print_game.c
+++ print_game.c
@@ -1,18 +1,31 @@
 #include "header.h"
 
+//game_over screen will be called when the player lose like when  he has 0 health point or he missed an quest//
+void game_over(){
+
+    ClearScreen();
+    printf("  _____          __  __ ______    ______      ________ _____   \n");
+    printf(" / ____|   /\\   |  \\/  |  ____|  / __ \\ \\    / /  ____|  __ \\  \n");
+    printf("| |  __   /  \\  | \\  / | |__    | |  | \\ \\  / /| |__  | |__) | \n");
+    printf("| | |_ | / /\\ \\ | |\\/| |  __|   | |  | |\\ \\/ / |  __| |  _  /  \n");
+    printf("| |__| |/ ____ \\| |  | | |____  | |__| | \\  /  | |____| | \\ \\  \n");
+    printf(" \\_____/_/    \\_\\_|  |_|______|  \\____/   \\/   |______|_|  \\_\\ \n");
+    exit(0);
+}
+
+//Introduction initialize all the importante  variable that are going to be possibly changed, it will also get the name of the player and check if it is too long//
 void Introduction(Player* knight){
 
   knight->itemsPlayer.herbs=0 ;
+  knight->itemsPlayer.woods=0;
   knight->score=0;
   knight->health = 100;
+  knight->dimensions=0;
+  knight->npcs.npc1=false;
+  knight->npcs.npc2=false;
   strcpy(knight->itemsPlayer.item1, ".");
   strcpy(knight->itemsPlayer.item2, ".");
   strcpy(knight->itemsPlayer.item3, ".");
-
-  
-  
-
-
   knight->posPlayer.pos_x = SIZE / 2;
   knight->posPlayer.pos_y = SIZE / 2;
 
@@ -24,64 +37,51 @@ void Introduction(Player* knight){
       }
       printf("%s, save us...please\n", knight->name);
       sleep(3);
-
 }
 
-
+//print_Overlay display all the important informations of the player like his health points, score, pieces of wood...//
 void print_Overlay(Player *a) {
   
-  printf("              \u2694  %s  \u2694",a->name);
+int Health_MAX = 100;
+if(a->health>100){
+  a->health=100;
+}
+const int Long = 20; // total length of the health bar
+int longHealth=0; // Length of the health bar based on current health points
 
-
-  int Health_MAX = 100;
-  const int Long = 20; // Longueur totale de la barre de vie
-  int longHealth =
-      (a->health * Long) / Health_MAX; // Longueur de la barre de vie en
-                                       // fonction des points de vie actuels
-  printf(
-      "\n                                                            Health : [");
-  for (int i = 0; i < Long; i++) {
-    if (i < longHealth) {
-      printf(ANSI_COLOR_RED
-             "|" ANSI_COLOR_RESET); // Caractère représentant la vie
-    } else {
-      printf(" "); // Caractère représentant l'espace vide
-    }
+printf("\n                                                            Health : [");
+longHealth = (a->health * Long) / Health_MAX; // Updating the length of the health bar
+for (int i = 0; i < Long; i++) {
+  if (i < longHealth) {
+    printf(ANSI_COLOR_RED "|" ANSI_COLOR_RESET); // health symbol
+  } else {
+    printf(" "); // empty space
   }
-  printf("]\n");
+}
+printf("]\n");
+//display of the overlay//
 
   printf("                                                            Score:%d\n",a->score);
-  printf("                                                            Inventory:\n");
-  printf("                                                            \U0001F33F x %d\n",a->itemsPlayer.herbs);
-  printf("                                                            %s\n",a->itemsPlayer.item1);
-  printf("                                                            %s\n",a->itemsPlayer.item2);
-  printf("                                                            %s\n",a->itemsPlayer.item3);
+  printf("                                                            Inventory:\n\n");
+  printf("                                                           \U0001F33F x %d\n",a->itemsPlayer.herbs);
+  printf("                                                           \U0001FAB5  x %d\n",a->itemsPlayer.woods);
+  printf("                                                           %s\n",a->itemsPlayer.item1);
+  printf("                                                           %s\n",a->itemsPlayer.item2);
+  printf("              \u2694  %s  \u2694",a->name);
+  printf("                                     %s\n",a->itemsPlayer.item3);
 
-  
-  if(a->health==0){
-    
-    printf(" _____          __  __ ______    ______      ________ _____   \n");
-    printf("/ ____|   /\\   |  \\/  |  ____|  / __ \\ \\    / /  ____|  __ \\  \n");
-    printf("| |  __   /  \\  | \\  / | |__    | |  | \\ \\  / /| |__  | |__) | \n");
-    printf("| | |_ | / /\\ \\ | |\\/| |  __|   | |  | |\\ \\/ / |  __| |  _  /  \n");
-    printf("| |__| |/ ____ \\| |  | | |____  | |__| | \\  /  | |____| | \\ \\  \n");
-    printf(" \\_____/_/    \\_\\_|  |_|______|  \\____/   \\/   |______|_|  \\_\\ \n");
-    
-    sleep(2);
-    exit(0)
-      
+  if(a->health==0){   
+    game_over();    
     }
 }
 
+//area_print display for a few second an 35*35 area of the map//
 void area_print(int tab[SIZE][SIZE], Player *player){
 
-     int window_x = player->posPlayer.pos_x -
-                 18; // Coordonnée x du coin supérieur gauche de la fenêtre
-  int window_y = player->posPlayer.pos_y -
-                 18; // Coordonnée y du coin supérieur gauche de la fenêtre
-
+  int window_x = player->posPlayer.pos_x -18; // x-coordinate of the top-left corner of the window
+  int window_y = player->posPlayer.pos_y -18; // y-coordinate of the top-left corner of the window
   
-    // Afficher la fenêtre
+    // display the window
     ClearScreen();
     printf("\n");
     for (int x = window_x; x < window_x + 35; x++) {
@@ -94,7 +94,7 @@ void area_print(int tab[SIZE][SIZE], Player *player){
       } else if (tab[x][y] == '@') {
         printf("\U0001F33F");
       } else if (tab[x][y] == '=') {
-        printf( "\U0001F64B"); // Ajout d'un espace après l'emoji
+        printf( "\U0001F64B"); 
       } else if (tab[x][y] == '^') {
         printf("\U0001F9D9");
       } else if (tab[x][y] == '#') {
@@ -111,12 +111,13 @@ void area_print(int tab[SIZE][SIZE], Player *player){
         printf("\U0001F3E1");
       } else if (tab[x][y] == 'X') {
         printf("\U0001F30A");
+      } else if (tab[x][y] == 'T') {
+        printf("\U0001F300");
       } else {
         printf("%c ", tab[x][y]);
       }
     }else {
-          printf("  "); // Afficher un espace pour les coordonnées en dehors de
-                        // la map
+          printf("  "); 
         }
       }
       printf("\n");
