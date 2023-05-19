e74f7ae17b5833323c82dd1551f197bd9a118759
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-19 17:12:47 +0200

    interactions.c

--- interactions.c
+++ interactions.c
@@ -1,37 +1,34 @@
 #include "header.h"
 
+
 //function that simulate the teleportation of the player on a random place in the map//
-void teleporter_interaction(int tab[SIZE][SIZE], Player *player) {
+void teleporter_interaction(int map[SIZE][SIZE], Player *player) {
   int teleporter_x = player->posPlayer.pos_x;
   int teleporter_y = player->posPlayer.pos_y + 1;
 
-  player->dimensions++; 
+  player->dimensions++;
+
     // player teleportation
   player->posPlayer.pos_x = rand() % (SIZE - 4) + 2;
   player->posPlayer.pos_y = rand() % (SIZE - 4) + 2;
   
 }
 
+
 //Npc2 quest which consist to take 3 teleporter//
 void npc2_quest(Player* knight) {
-    setbuf(stdout, NULL);//disable the buffer in order to force display
-   if(knight->dimensions!=3 && knight->npcs.npc2==false ){
-   printf("Hello... brave knight,when you will  cross 3 dimensions, i will help you...");
-   sleep(1);
-   fflush(stdout);
+   setbuf(stdout, NULL);//disable the buffer in order to force display
+   if(knight->dimensions!=3 && knight->npcs.npc2==false){
+        printf("Hello... brave knight,when you will  cross 3 dimensions, i will help you...");
+        sleep(1);
+        fflush(stdout);
    }
    else{
-    printf("Well played...");
-    knight->npcs.npc2=true;
-    sleep(1);
-    fflush(stdout);
+        printf("Well played...");
+        knight->npcs.npc2=true;
+        sleep(1);
+        fflush(stdout);  
    }
-   if(knight->npcs.npc2==true){
-    printf("aozi");
-    sleep(1);
-    fflush(stdout);
-   }
-   setvbuf(stdout, NULL, _IOLBF, BUFSIZ);  // enable the buffer to default   
 }
 
 //npc2 shop in which the player can be stronger//
@@ -40,107 +37,128 @@ void npc2_shop(Player* knight){
     printf("Tutu");
 }
 
+
+
 //The gift  interaction give randomly to the player +500 points of score,a game over...//
-void gift_interaction(int tab[SIZE][SIZE], Player *player){
-int random=rand()%6;
-sleep(1);
-printf("%d",random);
-switch(random){
+void gift_interaction(int map[SIZE][SIZE], Player *player){
+    int random=rand()%6;
+    sleep(1);
+    printf("%d",random);
 
-    case 0:
-    player->health+=50;
-    break;
 
-    case 1:
-    player->health-=50;
-    break;
+    switch(random){
 
-    case 2:
-    game_over();
-    break;
+        case 0:
+            player->health+=50;
+            break;
 
-    case 3:
-    player->score=0;
-    break;
+        case 1:
+            player->health-=50;
+            break;
 
-    case 4:
-    player->itemsPlayer.herbs=0;
-    break;
+        case 2:
+            game_over();
+            break;
 
-    case 5:
-    player->score+=500;
-    break;
+        case 3:
+            player->score=0;
+            break;
 
+        case 4:
+            player->itemsPlayer.herbs=0;
+            break;
+
+        case 5:
+            player->score+=500;
+            break;
+
+    }
 }
-}
 
-//This function will check which caractere is above the player and simulate the action associated with this caractere//
-int interaction(int board[SIZE][SIZE], Player* knight) {    
+//This function will check which caractere is above the player and simulate the action associated with this charactere//
+int interaction(int board[SIZE][SIZE], Player* knight) {
+    
     if(board[knight->posPlayer.pos_x][knight->posPlayer.pos_y] == '@' && knight->itemsPlayer.herbs <20) {
         knight->itemsPlayer.herbs++;
-         board[knight->posPlayer.pos_x][knight->posPlayer.pos_y] = '.';    
+         board[knight->posPlayer.pos_x][knight->posPlayer.pos_y] = '.';
+        
     } 
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '*'){
+
         if (strcmp(knight->itemsPlayer.item1, ".") == 0){
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
-            strcpy(knight->itemsPlayer.item1, "\u2694");//the player get the sword at item1 position
+            strcpy(knight->itemsPlayer.item1, "\u2694");
         }
+
         else if (strcmp(knight->itemsPlayer.item2, ".") == 0){
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
-            strcpy(knight->itemsPlayer.item2, "\u2694");//the player get the sword at item2 position
+            strcpy(knight->itemsPlayer.item2, "\u2694");
         }
+
         else if (strcmp(knight->itemsPlayer.item3, ".") == 0){
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
-            strcpy(knight->itemsPlayer.item3, "\u2694");//the player get the sword at item3 position
+            strcpy(knight->itemsPlayer.item3, "\u2694");
         }
     }
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '#'){
+
         if (strcmp(knight->itemsPlayer.item1, ".") == 0){
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
-            strcpy(knight->itemsPlayer.item1, "\U0001F6E1");//the player get the shield at item1 position
+            strcpy(knight->itemsPlayer.item1, "\U0001F6E1");
         }
+
         else if (strcmp(knight->itemsPlayer.item2, ".") == 0){
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
-            strcpy(knight->itemsPlayer.item2, "\U0001F6E1");//the player get the shield at item2 position
+            strcpy(knight->itemsPlayer.item2, "\U0001F6E1");
         }
+
         else if (strcmp(knight->itemsPlayer.item3, ".") == 0){
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
-            strcpy(knight->itemsPlayer.item3, "\U0001F6E1");//the player get the shield at item3 position
+            strcpy(knight->itemsPlayer.item3, "\U0001F6E1");
         }
     }
+
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '~'){
         board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
         
         gift_interaction(board,knight);
         sleep(2);
     }
+
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '!'){
         if(strcmp(knight->itemsPlayer.item1, "\u2694")==0 ||strcmp(knight->itemsPlayer.item2 , "\u2694")==0 || strcmp(knight->itemsPlayer.item3, "\u2694")==0){
-            knight->health-=30;//if the player has the sword he will kill the ennemy but he will lose  30 hp
+            knight->health-=30;
             knight->score+=50;
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y]='.';
         }
         else if((strcmp(knight->itemsPlayer.item1, "\u2694")==0 ||strcmp(knight->itemsPlayer.item2 , "\u2694")==0 || strcmp(knight->itemsPlayer.item3, "\u2694")==0)&&(strcmp(knight->itemsPlayer.item1, "\U0001F6E1")==0 ||strcmp(knight->itemsPlayer.item2 , "\U0001F6E1")==0 || strcmp(knight->itemsPlayer.item3, "\U0001F6E1")==0)) {
-            knight->health-=15;//if the player has the sword and the shield he will kill the ennemy but he will lose 15hp
+            knight->health-=15;
             knight->score+=50;
             board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y]='.';
-        }
+        } 
         else if(strcmp(knight->itemsPlayer.item1, "\U0001F6E1")==0 ||strcmp(knight->itemsPlayer.item2 , "\U0001F6E1")==0 || strcmp(knight->itemsPlayer.item3, "\U0001F6E1")==0){
             knight->health-=15;
-      }
-        else{                
-              knight->health-=50;//if the player dont have the sword and shield he will only lose 50hp
+        }
+        else{
+                
+              knight->health-=50;     
         }
     }
+
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == 'T'){
        teleporter_interaction(board,knight);      
     }
+
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == 'W' &&(strcmp(knight->itemsPlayer.item1, "\u2694")==0 ||strcmp(knight->itemsPlayer.item2 , "\u2694")==0 || strcmp(knight->itemsPlayer.item3, "\u2694")==0) && knight->itemsPlayer.woods<15 ){ 
         board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] ='.';
         knight->itemsPlayer.woods++;
+
     }
+
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '^'){
       npc2_quest(knight);
     }
+
     return 0;
 }
+
