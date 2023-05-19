e915d2ea9d5a1e55eabf0f393fb6746f1aa2c8af
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-19 17:03:32 +0200

    print_game.c

--- print_game.c
+++ print_game.c
@@ -2,7 +2,7 @@
 
 //game_over screen will be called when the player lose like when  he has 0 health point or he missed an quest//
 void game_over(){
-
+  
     ClearScreen();
     printf("  _____          __  __ ______    ______      ________ _____   \n");
     printf(" / ____|   /\\   |  \\/  |  ____|  / __ \\ \\    / /  ____|  __ \\  \n");
@@ -16,70 +16,79 @@ void game_over(){
 //Introduction initialize all the importante  variable that are going to be possibly changed, it will also get the name of the player and check if it is too long//
 void Introduction(Player* knight){
 
-  knight->itemsPlayer.herbs=0 ;
-  knight->itemsPlayer.woods=0;
-  knight->score=0;
-  knight->health = 100;
-  knight->dimensions=0;
-  knight->npcs.npc1=false;
-  knight->npcs.npc2=false;
-  strcpy(knight->itemsPlayer.item1, ".");
-  strcpy(knight->itemsPlayer.item2, ".");
-  strcpy(knight->itemsPlayer.item3, ".");
-  knight->posPlayer.pos_x = SIZE / 2;
-  knight->posPlayer.pos_y = SIZE / 2;
+    knight->itemsPlayer.herbs=0 ;
+    knight->itemsPlayer.woods=0;
+    knight->score=0;
+    knight->health = 100;
+    knight->dimensions=0;
+    knight->npcs.npc1=false;
+    knight->npcs.npc2=false;
+    knight->npcs.npc1_done=false;
+    knight->npcs.npc1_done=true;
+    knight->quest_seconds=60;
+    strcpy(knight->itemsPlayer.item1, ".");
+    strcpy(knight->itemsPlayer.item2, ".");
+    strcpy(knight->itemsPlayer.item3, ".");
 
-   printf("Welcome brave knight...We need you to save our kingdom...But, what's your name ?\n");
-      scanf("%s", knight->name);
-      while(strlen(knight->name)>12){
+    knight->posPlayer.pos_x = SIZE / 2;
+    knight->posPlayer.pos_y = SIZE / 2;
+
+    printf("Welcome brave knight...We need you to save our kingdom...But, what's your name ?\n");
+    scanf("%s", knight->name);
+    while(strlen(knight->name)>12){
         printf("Please enter a correct name\n");
         scanf("%s", knight->name);
       }
-      printf("%s, save us...please\n", knight->name);
-      sleep(3);
+    printf("%s, save us...please\n", knight->name);
+    sleep(3);
+
+
 }
 
 //print_Overlay display all the important informations of the player like his health points, score, pieces of wood...//
-void print_Overlay(Player *a) {
+void print_Overlay(Player *knight) {
   
-int Health_MAX = 100;
-if(a->health>100){
-  a->health=100;
+  int Health_MAX = 100;
+  if(knight->health>100){
+        knight->health=100;
 }
-const int Long = 20; // total length of the health bar
-int longHealth=0; // Length of the health bar based on current health points
+  const int Long = 20; // total length of the health bar
+  int longHealth=0; // Length of the health bar based on current health points
 
-printf("\n                                                            Health : [");
-longHealth = (a->health * Long) / Health_MAX; // Updating the length of the health bar
-for (int i = 0; i < Long; i++) {
-  if (i < longHealth) {
-    printf(ANSI_COLOR_RED "|" ANSI_COLOR_RESET); // health symbol
-  } else {
-    printf(" "); // empty space
+  printf("\n                                                            Health : [");
+  longHealth = (knight->health * Long) / Health_MAX; // Updating the length of the health bar
+  for (int i = 0; i < Long; i++) {
+        if (i < longHealth) {
+            printf(ANSI_COLOR_RED "|" ANSI_COLOR_RESET); // health symbol
+  }     else {
+            printf(" "); // empty space
   }
 }
-printf("]\n");
+  printf("]\n");
 //display of the overlay//
 
-  printf("                                                            Score:%d\n",a->score);
-  printf("                                                            Inventory:\n\n");
-  printf("                                                           \U0001F33F x %d\n",a->itemsPlayer.herbs);
-  printf("                                                           \U0001FAB5  x %d\n",a->itemsPlayer.woods);
-  printf("                                                           %s\n",a->itemsPlayer.item1);
-  printf("                                                           %s\n",a->itemsPlayer.item2);
-  printf("              \u2694  %s  \u2694",a->name);
-  printf("                                     %s\n",a->itemsPlayer.item3);
+  printf("                                                            Score:%d\n",knight->score);
 
-  if(a->health==0){   
-    game_over();    
+
+  printf("                                                            Inventory:\n\n");
+  printf("                                                           \U0001F33F x %d\n",knight->itemsPlayer.herbs);
+  printf("                                                           \U0001FAB5  x %d\n",knight->itemsPlayer.woods);
+  printf("              \u2694  %s  \u2694\n",knight->name);
+  printf("                                                           %s\n",knight->itemsPlayer.item1);
+  printf("                                                           %s\n",knight->itemsPlayer.item2);
+  printf("                                                           %s\n",knight->itemsPlayer.item3);
+ 
+    
+  if(knight->health==0){   
+      game_over();    
     }
 }
 
 //area_print display for a few second an 35*35 area of the map//
 void area_print(int tab[SIZE][SIZE], Player *player){
 
-  int window_x = player->posPlayer.pos_x -18; // x-coordinate of the top-left corner of the window
-  int window_y = player->posPlayer.pos_y -18; // y-coordinate of the top-left corner of the window
+    int window_x = player->posPlayer.pos_x -18; // x-coordinate of the top-left corner of the window
+    int window_y = player->posPlayer.pos_y -18; // y-coordinate of the top-left corner of the window
   
     // display the window
     ClearScreen();
@@ -90,36 +99,38 @@ void area_print(int tab[SIZE][SIZE], Player *player){
           if (x == player->posPlayer.pos_x && y == player->posPlayer.pos_y) {
             printf(ANSI_COLOR_YELLOW "P " ANSI_COLOR_RESET);
           } else if (tab[x][y] == '*') {
-        printf("\u2694 ");
-      } else if (tab[x][y] == '@') {
-        printf("\U0001F33F");
-      } else if (tab[x][y] == '=') {
-        printf( "\U0001F64B"); 
-      } else if (tab[x][y] == '^') {
-        printf("\U0001F9D9");
-      } else if (tab[x][y] == '#') {
-        printf("\U0001F6E1 ");
-      } else if (tab[x][y] == '~') {
-        printf("\U0001F381");
-      } else if (tab[x][y] == 'W') {
-        printf("\U0001FAB5 ");
-      } else if (tab[x][y] == 'R') {
-        printf("\U0001FAA8 ");
-      } else if (tab[x][y] == '!') {
-        printf("\U0001F9DF ");
-      } else if (tab[x][y] == 'H') {
-        printf("\U0001F3E1");
-      } else if (tab[x][y] == 'X') {
-        printf("\U0001F30A");
-      } else if (tab[x][y] == 'T') {
-        printf("\U0001F300");
-      } else {
-        printf("%c ", tab[x][y]);
-      }
+                printf("\u2694 ");
+          } else if (tab[x][y] == '@') {
+                 printf("\U0001F33F");
+          } else if (tab[x][y] == '=') {
+                 printf( "\U0001F64B"); 
+          } else if (tab[x][y] == '^') {
+                 printf("\U0001F9D9");
+          } else if (tab[x][y] == '#') {
+                printf("\U0001F6E1 ");
+          } else if (tab[x][y] == '~') {
+                printf("\U0001F381");
+          } else if (tab[x][y] == 'W') {
+                printf("\U0001FAB5 ");
+          } else if (tab[x][y] == 'R') {
+                printf("\U0001FAA8 ");
+          } else if (tab[x][y] == '!') {
+                printf("\U0001F9DF ");
+          } else if (tab[x][y] == 'H') {
+                printf("\U0001F3E1");
+          } else if (tab[x][y] == 'X') {
+                printf("\U0001F30A");
+          } else if (tab[x][y] == 'T') {
+                printf("\U0001F300");
+          } else {
+                printf("%c ", tab[x][y]);
+          }
     }else {
           printf("  "); 
         }
       }
       printf("\n");
     }
+
 }
+
