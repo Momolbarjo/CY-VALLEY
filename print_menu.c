f37047de3186fead8403424e6eaf55bab720019a
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-19 16:36:37 +0200

    print_menu.c

--- print_menu.c
+++ print_menu.c
@@ -11,13 +11,21 @@ int print_game_instructions() {
   printf("  's' - Move Down\n");
   printf("  'd' - Move Right\n\n");
   printf("- Your character is represented by 'P' on the game board.\n\n");
-  printf("- '\U0001F3E1' represents a house on the game board.\n\n");
-  printf("- '\U0001F33F' represents grass on the game board. You can farm on the grass to collect resources.\n\n");
-  printf("- Your character has a health bar represented by a percentage. Your health decreases when you encounter obstacles or enemies.\n\n");
-  printf("- Collect herbs ,it can be useful..\n\n");
-  printf("- Your objective is to explore the game world, collect resources, complete quests, and interact with NPCs to progress in the game.\n\n");
-  printf("- You can save your game progress by choosing the '2' option from the in-game menu.\n\n");
-  printf("- To exit the game, choose the '3' option from the in-game menu.\n\n");
+  printf("- 'H' represents a house on the game board. You need a key to enter "
+         "the house.\n\n");
+  printf("- '@' represents grass on the game board. You can farm on the grass "
+         "to collect resources.\n\n");
+  printf("- Your character has a health bar represented by a percentage. Your "
+         "health decreases when you encounter obstacles or enemies.\n\n");
+  printf("- Collect herbs to restore your health. Herbs can be found by "
+         "farming on grass or by completing quests.\n\n");
+  printf(
+      "- Your objective is to explore the game world, collect resources, "
+      "complete quests, and interact with NPCs to progress in the game.\n\n");
+  printf("- You can save your game progress by choosing the '2' option from "
+         "the in-game menu.\n\n");
+  printf(
+      "- To exit the game, choose the '3' option from the in-game menu.\n\n");
   printf("- Good luck and have fun playing the game!\n\n");
   sleep(6);
   ClearScreen();
@@ -26,7 +34,7 @@ int print_game_instructions() {
 
 int show_menu() {
   while (1) {
-   printf("\x1b[31m");
+    printf("\x1b[31m");
     printf("   _____          __      __   _ _                       _    .  ,   .           .  \n");
     printf("  / ____|         \\ \\    / /  | | |                  *  / \\_ *  / \\_      _  *        *   /\\'__        *\n");
     printf(" | |    _   _ _____\\ \\  / /_ _| | | ___ _   _          /    \\  /    \\,   ((        .    _/  /  \\  *'\n");
@@ -42,7 +50,11 @@ int show_menu() {
     printf("\033[0;36m");
     printf("                                      2. \033[0m\033[1;33m%s\033[0m\U0001F4DC\n", "Keys");
     printf("\033[0;36m");
-    printf("                                         3. \033[0m\033[1;33m%s\033[0m\U0001F6AA\n", "Leave");
+    printf("                                         3. \033[0m\033[1;33m%s\033[0m\U0001F4BE\n", "Resume game");
+    printf("\033[0;36m");
+    printf("                                            4. \033[0m\033[1;33m%s\033[0m\U0001F6AA\n", "Leave");
+
+
     int choice;
     scanf("%d", &choice);
     getchar(); // Consume newline character from input buffer
@@ -56,6 +68,9 @@ int show_menu() {
       return 2;
 
     case 3:
+    return 3;
+
+    case 4:
       exit(0);
 
     default:
