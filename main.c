d94d024986cdeeb238412f836db45b4ad2c42bcc
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-19 16:37:09 +0200

    main.c

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
 #include "header.h"
 
-void main() {
+int main() {
 
-  open_website_part(3);//Open an URL and here is a YouTube URL to play some music.
-
+  open_website_part(3); // Ouverture d'une URL
   int board[SIZE][SIZE];
   Player knight;
   Position player_position;
   Inventory player_inventory;
   Quests player_quests;
-  
-
-  Secure_Boot(&knight,&player_inventory,&player_position,&player_quests);
+  Secure_Boot(&knight, &player_inventory, &player_position, &player_quests);
 
   while (1) {
     int choice = show_menu();
@@ -21,16 +18,17 @@ void main() {
       initialise_board(board);
       randomizer(board);
       move_window(board, &knight);
-     
-      
-      
     } else if (choice == 2) {
-        ClearScreen();
-        print_game_instructions();
+      ClearScreen();
+      print_game_instructions();
 
-    } else if (choice == 3) {
-        ClearScreen();
+    }
+    else if (choice == 3) {
+      ClearScreen();
+      chargerPartie(board,&knight, "partie.bin", "carte.bin");
+      move_window(board, &knight);
     }
   }
+  return 0;
 }
 
