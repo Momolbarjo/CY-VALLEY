f54641e25f864e035b8cbe90424dbc9de26e4dde
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-20 19:37:08 +0200

    window.c

--- window.c
+++ window.c
@@ -2,9 +2,9 @@
 
 //move_window display the game window that make sure to move following the movement of the player//
 int move_window(int map[SIZE][SIZE], Player *player) {
-  while (1) {
-    int window_x = player->posPlayer.pos_x - 15; // x-coordinate of the top-left corner of the window
-    int window_y = player->posPlayer.pos_y -25; // y-coordinate of the top-left corner of the window
+while (1) {
+  int window_x = player->posPlayer.pos_x - 15; // x-coordinate of the top-left corner of the window
+  int window_y = player->posPlayer.pos_y -25; // y-coordinate of the top-left corner of the window
 
   
     // display the window
@@ -18,34 +18,36 @@ int move_window(int map[SIZE][SIZE], Player *player) {
           if (x == player->posPlayer.pos_x && y == player->posPlayer.pos_y) {
             printf(ANSI_COLOR_YELLOW "P " ANSI_COLOR_RESET);
           } else if (map[x][y] == '*') {
-                printf("\u2694 ");
+        printf("\u2694 ");
       } else if (map[x][y] == '@') {
-                printf("\U0001F33F");
+        printf("\U0001F33F");
       } else if (map[x][y] == '=') {
-                printf( "\U0001F64B"); // Ajout d'un espace après l'emoji
+        printf( "\U0001F64B"); // Ajout d'un espace après l'emoji
       } else if (map[x][y] == '^') {
-                printf("\U0001F9D9");
+        printf("\U0001F9D9");
       } else if (map[x][y] == '#') {
-                 printf("\U0001F6E1 ");
+        printf("\U0001F6E1 ");
       } else if (map[x][y] == 'W') {
-                 printf("\U0001FAB5 ");
+        printf("\U0001FAB5 ");
       } else if (map[x][y] == 'R') {
-                 printf("\U0001FAA8 ");
+        printf("\U0001FAA8 ");
       } else if (map[x][y] == '~') {
-                 printf("\U0001F381");
+        printf("\U0001F381");
       } else if (map[x][y] == '!') {
-                 printf("\U0001F9DF ");
+        printf("\U0001F9DF ");
       } else if (map[x][y] == 'H') {
-                 printf("\U0001F3E1");
+        printf("\U0001F3E1");
       } else if (map[x][y] == 'T') {
-                 printf("\U0001F300");
+        printf("\U0001F300");
       } else if (map[x][y] == 'X') {
-                 printf("\U0001F30A");
-      }  else {
-                 printf("%c ", map[x][y]);
+        printf("\U0001F30A");
+      } else if (map[x][y] == '%') {
+        printf(".");
+      } else {
+        printf("%c ", map[x][y]);
       }
     }else {
-         printf("  "); 
+          printf("  "); 
         }
       }
       printf("\n");
@@ -64,10 +66,10 @@ int move_window(int map[SIZE][SIZE], Player *player) {
     switch (input[0]) {
     case 'z':
        if (player->posPlayer.pos_x > 2 && (map[player->posPlayer.pos_x-1][player->posPlayer.pos_y] == '.' || map[player->posPlayer.pos_x-1][player->posPlayer.pos_y] == '@')) {
-          player->posPlayer.pos_x--;
-          window_x--;
-          player->score++;
-      }
+        player->posPlayer.pos_x--;
+        window_x--;
+        player->score++;
+    }
     else if(player->posPlayer.pos_x > 2 && map[player->posPlayer.pos_x-1][player->posPlayer.pos_y] == 'R') {
         if(map[player->posPlayer.pos_x-2][player->posPlayer.pos_y] == '.') {
             // Push the box to the new position
@@ -79,11 +81,11 @@ int move_window(int map[SIZE][SIZE], Player *player) {
         }
     }
       break;
-      case 'q':
+    case 'q':
       if (player->posPlayer.pos_y > 2 && (map[player->posPlayer.pos_x ][player->posPlayer.pos_y-1]=='.' || map[player->posPlayer.pos_x ][player->posPlayer.pos_y-1]=='@')) {
-            player->posPlayer.pos_y--;
-            window_y--;
-            player->score++;
+        player->posPlayer.pos_y--;
+        window_y--;
+        player->score++;
       }
       else if(player->posPlayer.pos_x > 2 &&  map[player->posPlayer.pos_x ][player->posPlayer.pos_y-1]=='R'){
       if(map[player->posPlayer.pos_x][player->posPlayer.pos_y-2] == '.') {
@@ -99,52 +101,54 @@ int move_window(int map[SIZE][SIZE], Player *player) {
       break;
     case 's':
       if (player->posPlayer.pos_x < SIZE - 3 && (map[player->posPlayer.pos_x+1 ][player->posPlayer.pos_y]=='.' || map[player->posPlayer.pos_x+1 ][player->posPlayer.pos_y]=='@')) {
-            player->posPlayer.pos_x++;
-            window_x++;
-            player->score++; 
+        player->posPlayer.pos_x++;
+        window_x++;
+        player->score++; 
       }
       else if( player->posPlayer.pos_x < SIZE - 3 &&  map[player->posPlayer.pos_x+1 ][player->posPlayer.pos_y]=='R'){
-          if(map[player->posPlayer.pos_x+2][player->posPlayer.pos_y] == '.') {
-                // Push the box represented by the rock to the new position
-                map[player->posPlayer.pos_x+2][player->posPlayer.pos_y] = 'R';
-                map[player->posPlayer.pos_x+1][player->posPlayer.pos_y] = '.';
-                player->posPlayer.pos_x++;
-                window_x++;
-                player->score+=5; 
+      if(map[player->posPlayer.pos_x+2][player->posPlayer.pos_y] == '.') {
+            // Push the box represented by the rock to the new position
+            map[player->posPlayer.pos_x+2][player->posPlayer.pos_y] = 'R';
+            map[player->posPlayer.pos_x+1][player->posPlayer.pos_y] = '.';
+            player->posPlayer.pos_x++;
+            window_x++;
+            player->score+=5; 
         }
       }
       break;
     case 'd':
       if (player->posPlayer.pos_y < SIZE - 3 && (map[player->posPlayer.pos_x ][player->posPlayer.pos_y+1]=='.' || map[player->posPlayer.pos_x ][player->posPlayer.pos_y+1]=='@')) {
-           player->posPlayer.pos_y++;
-           window_y++;
-           player->score++; 
-
+        player->posPlayer.pos_y++;
+        window_y++;
+        player->score++; 
+        
       }
       else if( player->posPlayer.pos_x < SIZE - 3 &&  map[player->posPlayer.pos_x ][player->posPlayer.pos_y+1]=='R'){
-          if(map[player->posPlayer.pos_x][player->posPlayer.pos_y+2] == '.') {
-             // Push the box represented by the rock to the new position
-              map[player->posPlayer.pos_x][player->posPlayer.pos_y+2] = 'R';
-              map[player->posPlayer.pos_x][player->posPlayer.pos_y+1] = '.';
-              player->posPlayer.pos_y++;
-              window_y++;
-              player->score+=5; 
-
+        if(map[player->posPlayer.pos_x][player->posPlayer.pos_y+2] == '.') {
+           // Push the box represented by the rock to the new position
+            map[player->posPlayer.pos_x][player->posPlayer.pos_y+2] = 'R';
+            map[player->posPlayer.pos_x][player->posPlayer.pos_y+1] = '.';
+            player->posPlayer.pos_y++;
+            window_y++;
+            player->score+=5; 
+            
         }
       }
       
       break;
       case 'e':
       ClearScreen();
-      printf("What do you want to do?\n1.resume\n2.save\n3.Map\n4.Return to the menu\n"); //display a menu and wait the input of the player//
+      printf(
+          "What do you want to do?\n1.resume\n2.save\n3.Map\n4.Return to the menu\n"); //display a menu and wait the input of the player//
       char choice[2];
-      while(getchar() != '\n'); // Clear the input Buffer
-      fgets(choice, 2, stdin);
+    while(getchar() != '\n'); // Clear the input Buffer
+    fgets(choice, 2, stdin);
      
-      switch(choice[0]){
+     switch(choice[0]){
      	
-     	  case '1':
-     	  break;
+     	case '1':
+     	
+     	break;
      	
        	case '2':
        	
@@ -167,9 +171,9 @@ int move_window(int map[SIZE][SIZE], Player *player) {
        ClearScreen();
        return 0;  
        }  
-       default:
-       printf("Invalid input.\n");
-       break;
+      default:
+      printf("Invalid input.\n");
+      break;
     }
   }
   }
