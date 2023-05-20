3d89727e432bfab3b0b79ba804e6e29383470b8d
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2023-05-20 19:34:38 +0200

    interactions.c

--- interactions.c
+++ interactions.c
@@ -3,20 +3,24 @@
 
 //function that simulate the teleportation of the player on a random place in the map//
 void teleporter_interaction(int map[SIZE][SIZE], Player *player) {
-  int teleporter_x = player->posPlayer.pos_x;
-  int teleporter_y = player->posPlayer.pos_y + 1;
 
   player->dimensions++;
 
-    // player teleportation
-  player->posPlayer.pos_x = rand() % (SIZE - 4) + 2;
-  player->posPlayer.pos_y = rand() % (SIZE - 4) + 2;
-  
+  // Looking for a cell available
+  int new_x, new_y;
+  do {
+    new_x = rand() % (SIZE - 4) + 2;
+    new_y = rand() % (SIZE - 4) + 2;
+  } while (map[new_x][new_y] != '.' && map[new_x][new_y] != '@');
+
+  // update of the player coordinates
+  player->posPlayer.pos_x = new_x;
+  player->posPlayer.pos_y = new_y;
 }
 
 
 //Npc2 quest which consist to take 3 teleporter//
-void npc2_quest(Player* knight) {
+void npc2_quest(int map[SIZE][SIZE], Player* knight) {
    int secret_posx=knight->posPlayer.pos_x-3;
    int secret_posy=knight->posPlayer.pos_y+5;
 
@@ -24,6 +28,7 @@ void npc2_quest(Player* knight) {
    //step 1:
    if(knight->dimensions!=3 && knight->npcs.npc2_step1==false){
         printf("Hello... brave knight,when you will  cross 3 dimensions,come back...");
+        knight->npcs.timer_start=true;
         sleep(2);
         fflush(stdout);
    }//step1 done:
@@ -35,9 +40,9 @@ void npc2_quest(Player* knight) {
    }
 
    if( knight->npcs.npc2_step1==true && knight->npcs.npc2_step2==false){
-        printf("\u2192 + \u2192 + \u2192 + \u2192 + \u2191 + \u2191 +\u2191 ...");
+        printf("\u2192 + \u2192 + \u2192 + \u2192 + \u2192 + \u2191 +\u2191+\u2191 ...");
         sleep(2);
-        if(knight->posPlayer.pos_x==secret_posx && knight->posPlayer.pos_y==secret_posy){
+        if(map[knight->posPlayer.pos_x][knight->posPlayer.pos_y]==map[secret_posx][secret_posy] || map[knight->posPlayer.pos_x][knight->posPlayer.pos_y]==map[secret_posx-1][secret_posy] ){
             knight->npcs.npc2_step2=true;
             sleep(1);
         }
@@ -47,6 +52,7 @@ void npc2_quest(Player* knight) {
    else if (knight->npcs.npc2_step2==true){
         printf("Impressive...");
         sleep(2);
+        npc2_shop(knight);
         fflush(stdout);
    }
 }
@@ -54,13 +60,85 @@ void npc2_quest(Player* knight) {
 //npc2 shop in which the player can be stronger//
 void npc2_shop(Player* knight){
 
-    printf("Tutu");
+    int shop = 1;
+
+    while (shop) {
+        printf("Welcome to my shop, what do you want?\n");
+        printf("1. buy items\n");
+        printf("2. leave the shop\n");
+        
+
+        int choice;
+        scanf("%d", &choice);
+        ClearScreen();
+
+        switch (choice) {
+        
+            case 1:
+                printf("what do you want to buy?\n");
+                printf("1. potion of 20  health points (cost : 5\U0001F33F )\n");
+                printf("2. +200 score points (cost : 5\U0001F33F  + 3\U0001FAB5)\n");
+                printf("3. END_GAME (cost : 2500 points )\n");
+                
+
+
+                int buyItem;
+                scanf("%d", &buyItem);
+                ClearScreen();
+
+                switch (buyItem) {
+                    case 1:
+                        if (knight->itemsPlayer.herbs >= 5) {
+                            knight->itemsPlayer.herbs -= 5;
+                            knight->health+=20;
+                            printf("You've got + 20 health points ! \n");
+                        } else {
+                            printf("You dont have enough \U0001F33F ...\n");
+                        }
+                        break;
+
+                    case 2:
+                        if (knight->itemsPlayer.herbs >= 5 && knight->itemsPlayer.woods >= 3 ) {
+                            knight->itemsPlayer.herbs -= 5;
+                            knight->itemsPlayer.woods -= 3;
+                            knight->score+=200;
+                            printf("You've got + 200  score points! \n");
+                        } else {
+                            printf("You dont have enough resources...\n");
+                        }
+                        break;
+
+                    case 3:
+                        if (knight->score >= 2500) {
+                            knight->score-= 2500;
+                            printf("Well played, you win thanks to you score points!\n");
+                        } else {
+                            printf("You dont have enough score points ...\n");
+                        }
+                        break;
+
+                    default:
+                        printf("Invalid choice.\n");
+                        break;
+                }
+                break;
+
+            case 2:
+                shop = 0;
+                printf("You are leaving the shop...\n");
+                break;
+
+            default:
+                printf("Invalid choice.\n");
+                break;
+        }
+    }
 }
 
 
 
 //The gift  interaction give randomly to the player +500 points of score,a game over...//
-void gift_interaction(int map[SIZE][SIZE], Player *player){
+void gift_interaction( Player *player){
     int random=rand()%6;
     sleep(1);
     printf("%d",random);
@@ -141,7 +219,7 @@ int interaction(int board[SIZE][SIZE], Player* knight) {
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '~'){
         board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] = '.';
         
-        gift_interaction(board,knight);
+        gift_interaction(knight);
         sleep(2);
     }
 
@@ -178,10 +256,8 @@ int interaction(int board[SIZE][SIZE], Player* knight) {
     }
 
     if(board[knight->posPlayer.pos_x-1][knight->posPlayer.pos_y] == '^'){
-      npc2_quest(knight);
+      npc2_quest(board,knight);
     }
-
 
     return 0;
 }
-
